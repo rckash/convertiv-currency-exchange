62cc8691ffa9339ea8fec1f1469c122c217ffc54
rckash <rckashyapcpe@gmail.com>
2024-05-20 13:00:05 +0800

    Add all HTML elements needed and basic styling for why-choose-us section

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 alert("Hello World!");
 
-const API_KEY =
-  "https://api.currencybeacon.com/v1/latestvIVCKDc3sl3JkTM7F9KiqZhbZ3RBgUbl";
+const API_KEY = "vIVCKDc3sl3JkTM7F9KiqZhbZ3RBgUbl";
 const API_URL = "https://api.currencybeacon.com/v1/latest";
 var baseCurrency = "USD";
-var symbols = "EUR,PHP,JPY,GBP,AUD";
+var symbols =
+  "PHP,EUR,JPY,GBP,AUD,CAD,CHF,CNH,HKD,NZD,SGD,SEK,KRW,NOK,NZD,INR,MXN,TWD,ZAR";
 
 async function checkCurrency() {
   const response = await fetch(
-    API_URL + `?api_key=${API_KEY}` + `&base={}` + `&symbols=${symbols}`
+    API_URL +
+      `?api_key=${API_KEY}` +
+      `&base=${baseCurrency}` +
+      `&symbols=${symbols}`
   );
   var data = await response.json();
+
+  console.log(data);
 }
+
+// checkCurrency();
