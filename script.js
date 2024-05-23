366b06a0c8b5bee4c2b5be2762d339b442fbbb10
rckash <rckashyapcpe@gmail.com>
2024-05-23 19:29:31 +0800

    Add pagination with DOM manipulation functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // API
   const API_KEY = "vIVCKDc3sl3JkTM7F9KiqZhbZ3RBgUbl";
   const API_URL = "https://api.currencybeacon.com/v1/latest";
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log(myArray);
 
       const tableBody = document.getElementById("currency-table");
-      tableBody.innerHTML = '';
+      tableBody.innerHTML = "";
 
       for (const [currency, rate] of Object.entries(myArray)) {
         const rowElement = document.createElement("tr");
@@ -35,26 +35,55 @@ document.addEventListener("DOMContentLoaded", function() {
         rowElement.appendChild(currencyCell);
 
         const sellCell = document.createElement("td");
-        sellCell.textContent = rate.toFixed(4); 
+        sellCell.textContent = rate.toFixed(4);
         rowElement.appendChild(sellCell);
 
-        const buyRate = (rate * 1.05).toFixed(4); 
+        const buyRate = (rate * 1.05).toFixed(4);
         const buyCell = document.createElement("td");
         buyCell.textContent = buyRate;
         rowElement.appendChild(buyCell);
 
-
         const trendCell = document.createElement("td");
-        trendCell.textContent = "Trend"; 
+        trendCell.textContent = "Trend";
         rowElement.appendChild(trendCell);
 
-    
         tableBody.appendChild(rowElement);
       }
     } catch (error) {
-      console.error('Error fetching exchange rates:', error);
+      console.error("Error fetching exchange rates:", error);
     }
   }
 
   checkRates();
 });
+
+let link = document.getElementsByClassName("link");
+let currentValue = 1;
+
+function activeLink() {
+  for (l of link) {
+    l.classList.remove("active");
+  }
+  event.target.classList.add("active");
+  currentValue = event.target.value;
+}
+
+function backBtn() {
+  if (currentValue > 1) {
+    for (l of link) {
+      l.classList.remove("active");
+    }
+    currentValue--;
+    link[currentValue - 1].classList.add("active");
+  }
+}
+
+function nextBtn() {
+  if (currentValue < 3) {
+    for (l of link) {
+      l.classList.remove("active");
+    }
+    currentValue++;
+    link[currentValue - 1].classList.add("active");
+  }
+}
